afc54d180516d984e3e5fb9e71537d33ad3f0058
Maria <kuzmenkomkbk@gmail.com>
2022-12-07 14:09:36 +0300

    HARD STAT poyasneniya

--- Sem5_HARD_STAT/Program.cs
+++ Sem5_HARD_STAT/Program.cs
@@ -7,7 +7,7 @@
 int[] FillArray(int size)
 {
     int[] SomeArr = new int[size];
-    for (int i = 0; i < size; i++) SomeArr[i] = new Random().Next(-10, 21);
+    for (int i = 0; i < size; i++) SomeArr[i] = new Random().Next(1, 21);
     return SomeArr;
 }
 
@@ -15,6 +15,15 @@ void PrintArray(int[] arr)
 {
     foreach (int el in arr) Console.Write($"{el} ");
     Console.WriteLine();
+}
+void PrintArr(double[] arr,int[]array)
+{
+    Console.WriteLine($"Максимальный элемент массива равен {FindMax(array)}");
+    Console.WriteLine($"Индекс максимального элемента массива равен {FindMaxIndex(array, FindMax(array))}");
+    Console.WriteLine($"Минимальный элемент массива равен {FindMin(array)}");
+    Console.WriteLine($"Индекс минимального элемента массива равен {FindMinIndex(array, FindMin(array))}");
+    Console.WriteLine($"Среднее арифметическое всех элементов массива равно {ArithmeticMean(array)}");
+    // foreach (double el in arr) Console.Write($"{el} ");
 }
 
 int FindMax(int[] arr)
@@ -29,9 +38,10 @@ int FindMax(int[] arr)
 
 int FindMaxIndex(int[] arr, int max)
 {
+    int MaxIndex = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] == max) int MaxIndex = i;
+        if (arr[i] == max) MaxIndex = i;
     }
     return MaxIndex;
 }
@@ -47,28 +57,31 @@ int FindMin(int[] arr)
 
 int FindMinIndex(int[] arr, int min)
 {
+    int MinIndex = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] == min) int MinIndex = i;
+        if (arr[i] == min) MinIndex = i;
     }
     return MinIndex;
 }
 
-int ArithmeticMean(int[] arr)
+double ArithmeticMean(int[] arr)
 {
-    int arithmetic = 0;
+    double arithmetic = 0;
     for (int i = 0; i < arr.Length; i++)
     {
         arithmetic += arr[i];
     }
     arithmetic /= arr.Length;
+    return arithmetic;
 }
 try
 {
-    int len = Convert.ToInt32(Console.ReadLine());
+    int len = 10;
     int[] Arr = FillArray(len);
     PrintArray(Arr);
-    int[] array = new int[max, maxind, min, minind, arifm];
+    double[] NewArr = {FindMax(Arr), FindMaxIndex(Arr,FindMax(Arr)), FindMin(Arr), FindMinIndex(Arr,FindMin(Arr)), ArithmeticMean(Arr)};
+    PrintArr(NewArr,Arr);
 }
 catch
 {
